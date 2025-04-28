d3267a7ce8556fd1f03743ca573e9631c1d1df98
WILMER IMEMDOZA <wmendozar.cief.pow@gmail.com>
2025-04-28 10:07:42 +0200

    Creo la ruta de editar editoriales y el formulario

--- routes/editorialesRoutes.js
+++ routes/editorialesRoutes.js
@@ -11,4 +11,11 @@ router.post('/crear', editorialesController.createEditorial);
 // Borrar editorial
 router.get('/eliminar/:id', editorialesController.deleteEditorial);
 
+// Mostrar formulario de edición
+router.get('/editar/:id', autoresController.editEditorialForm);
+
+// Guardar cambios
+router.post('/editar/:id', autoresController.updateEtorial);
+
+
 module.exports = router;
